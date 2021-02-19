152d6f4c81065e8ad50645b14706d324d3aa8a00
Danielle Koski <dkoski4@yahoo.com>
2021-02-19 11:49:03 -0800

    cleaned up index.html, added new src with app.js, index.js, components with products

---
+++ src/Products/Products.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Grid from "@material-ui/core";
+
+const Products = () => {
+  <main>
+    <Grid containter justify="center" spacing={4}></Grid>
+  </main>;
+};
+
+export default Products;
